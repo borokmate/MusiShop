b82f2cf7fd3704f76739d8c9b7c456690331c044
borokmate <borokmate@gmail.com>
2026-04-10 09:00:00 +0000

    first commit

---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Quic;
+using System.Text;
+
+namespace BRUHWEGOOD
+{
+    public class Product
+    {
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public string Type { get; set; }
+        public Product(DateTime date, string name, int price, int quantity, string type)
+        {
+            Date = date;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Type = type;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace MusiShop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
