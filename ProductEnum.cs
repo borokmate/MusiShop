22c6048facc8522153aeecb9b0cca44c9f9d2972
PITRv1 <petersandor.toth@gmail.com>
2026-04-11 07:50:46 +0000

    added other product classes

---
+++ ProductEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BRUHWEGOOD
+{
+    public enum Products
+    {
+        Vinyl,
+        CD
+    }
+}
---
+++ Vinyl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BRUHWEGOOD
+{
+    //public class Vinyl : Product
+    //{
+    //    public DateTime Date { get; set; }
+    //    public string Name { get; set; }
+    //    public int Price { get; set; }
+    //    public int Quantity { get; set; }
+    //}
+}
