c5da55e2b7d132fe55cad69f70d5979e76cabc7b
borokmate <borokmate@gmail.com>
2026-04-18 22:36:55 +0000

    get menu item without clearing

--- Menu.cs
+++ Menu.cs
@@ -94,5 +94,109 @@ namespace MusiShop
             }
             while (key.Key != ConsoleKey.Enter);
         }
+        private static int GetMenuItemNoClear(string[] menu)
+        {
+            var pos = Console.GetCursorPosition();
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.SetCursorPosition(pos.Left, pos.Top);
+                PrintMenu(menu);
+                key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        do
+                        {
+                            if (0 < currItem) currItem--;
+                            else currItem = menu.Length - 1;
+                        } while (menu[currItem].StartsWith('['));
+                        break;
+                    case ConsoleKey.DownArrow:
+                        do
+                        {
+                            if (currItem < menu.Length - 1) currItem++;
+                            else currItem = 0;
+                        } while (menu[currItem].StartsWith('['));
+                        break;
+                    case ConsoleKey.Spacebar:
+                        currItem += 10;
+                        currItem = Math.Clamp(currItem, 0, menu.Length - 1);
+                        break;
+                    case ConsoleKey.Escape:
+                        currItem = -1;
+                        return 0;
+                    default:
+                        int found = Array.FindIndex(menu, x => x[0] == key.KeyChar);
+                        if (found != -1) currItem = found;
+                        break;
+                }
+            }
+            while (key.Key != ConsoleKey.Enter);
+            int offset = 0;
+            for (int i = 0; i < currItem; i++)
+            {
+                if (menu[i].StartsWith('[')) offset++;
+            }
+            currItem -= offset;
+            return offset;
+        }
+
+        private static int GetMenuItemNoClear(string[] menu, string endingText)
+        {
+            var pos = Console.GetCursorPosition();
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.SetCursorPosition(pos.Left, pos.Top);
+                PrintMenu(menu);
+                Console.WriteLine(endingText);
+                key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        do
+                        {
+                            if (0 < currItem) currItem--;
+                            else currItem = menu.Length - 1;
+                        } while (menu[currItem].StartsWith('['));
+                    break;
+                    case ConsoleKey.DownArrow:
+                        do
+                        {
+                            if (currItem < menu.Length - 1) currItem++;
+                            else currItem = 0;
+                        } while (menu[currItem].StartsWith('['));
+                        break;
+                    case ConsoleKey.Spacebar:
+                        currItem += 10;
+                        currItem = Math.Clamp(currItem, 0, menu.Length - 1);
+                        break;
+                    case ConsoleKey.Escape:
+                        currItem = -1;
+                        return 0;
+                    default:
+                        int found;
+                        if (key.KeyChar == ':')
+                        {
+                            char nextChar = Console.ReadKey().KeyChar;
+                            found = Array.FindIndex(menu, x => x[1] == nextChar && x[0] == '[');
+                            if (found != -1) found++;
+                        }
+                        else
+                            found = Array.FindIndex(menu, x => x[2] == key.KeyChar);
+                        if (found != -1) currItem = found;
+                        break;
+                }
+            }
+            while (key.Key != ConsoleKey.Enter);
+            int offset = 0;
+            for (int i = 0; i < currItem; i++)
+            {
+                if (menu[i].StartsWith('[')) offset++;
+            }
+            currItem -= offset;
+            return offset;
+        }
     }
 }
