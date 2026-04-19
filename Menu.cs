447302529802002154a7c5e8f042e1ab3538f262
borokmate <borokmate@gmail.com>
2026-04-19 21:27:41 +0000

    added menu funcs

--- Menu.cs
+++ Menu.cs
@@ -198,5 +198,311 @@ namespace MusiShop
             currItem -= offset;
             return offset;
         }
+        private static void CheckBacklog()
+        {
+            
+            currItem = 1;
+            while (true)
+            {
+                Console.Clear();
+                var products = Parser.ReadProducts();
+                var grouped = products.GroupBy(x => x.Type).OrderBy(g => g.Key);
+
+                var menuItems = new List<string>();
+
+                foreach (var group in grouped)
+                {
+                    menuItems.Add($"[{group.Key}]"); // header
+                    foreach (var product in group.OrderBy(x => x.Name))
+                    {
+                        menuItems.Add("  " + product.Name);
+                    }
+                }
+                int offset = GetMenuItemNoClear(menuItems.ToArray(), "Press escape to exit");
+                if (currItem == -1)
+                {
+                    currItem = 0;
+                    return;
+                }
+
+                Console.Clear();
+                Console.WriteLine("Chosen product");
+                PrintProduct(products[currItem]);
+                currItem += offset;
+            }
+        }
+        private static void PrintProduct(Product product, bool space = true)
+        {
+            Console.WriteLine("Date: " + product.Date);
+            Console.WriteLine("Name: " + product.Name);
+            Console.WriteLine("Price: " + product.Price);
+            Console.WriteLine("Quantity: " + product.Quantity);
+            Console.WriteLine("Type: " + product.Type);
+            if (space) WaitForSpace();
+        }
+        private static void AddNewProduct()
+        {
+            currItem = 0;
+            Console.Clear();
+            Console.WriteLine("What should the type be?");
+            var menu = new List<string>();
+            menu.AddRange(Parser.GetTypes());
+            menu.Add("New type");
+            GetMenuItemNoClear(menu.ToArray());
+            if (currItem == -1)
+            {
+                currItem = 0;
+                return;
+            }
+            string type;
+            if (currItem == menu.Count - 1)
+            {
+                type = GetString("new type: ");
+            }
+            else
+            {
+                type = menu[currItem];
+            }
+            var name = GetString("product name: ");
+            if (Parser.ReadProducts().Where(x => x.Name == name && x.Type == type).Count() > 0)
+            {
+                Console.WriteLine("This product already exists!");
+                Console.Write("Add to quantity? (y/n): ");
+                if (Console.ReadLine() == "n")
+                {
+                    return;
+                }
+                else
+                {
+                    foreach (var prod in Parser.ReadProducts().Where(x => x.Name == name && x.Type == type))
+                    {
+                        prod.Quantity++;
+                        Parser.ReplaceProduct(prod.Name, prod.Type, prod);
+                    }
+                    WaitForSpace();
+                    return;
+                }
+            }
+            int price = GetANumber("Price", 0);
+            int quantity = GetANumber("Quantity", 1);
+            DateTime date = GetADate("Release date", 0);
+            Product product = new Product(date, name, price, quantity, type);
+            Parser.WriteProduct(product);
+            Console.WriteLine("Successfully added new product;");
+            WaitForSpace();
+        }
+        private static void EditProducts()
+        {
+            string[] productMenu;
+            while (true)
+            {
+                Console.Clear();
+                var products = Parser.ReadProducts();
+                var grouped = products.GroupBy(x => x.Type).OrderBy(g => g.Key);
+
+                var menuItems = new List<string>();
+
+                foreach (var group in grouped)
+                {
+                    menuItems.Add($"[{group.Key}]"); // header
+                    foreach (var product in group.OrderBy(x => x.Name))
+                    {
+                        menuItems.Add("  " + product.Name);
+                    }
+                }
+                int originalOffset = GetMenuItemNoClear(menuItems.ToArray(), "Press escape to exit");
+                if (currItem == -1)
+                {
+                    currItem = 0;
+                    return;
+                }
+                var itemIndex = currItem;
+                var originalName = products[itemIndex].Name;
+                var originalType = products[itemIndex].Type;
+                currItem = 0;
+                while (true)
+                {
+                    Console.Clear();
+                    productMenu = new string[]
+                    {
+                        $"Date: {products[itemIndex].Date}",
+                        $"Name: {products[itemIndex].Name}",
+                        $"Price: {products[itemIndex].Price}",
+                        $"Quantity: {products[itemIndex].Quantity}",
+                        $"Type: {products[itemIndex].Type}"
+                    };
+                    Console.WriteLine("Chosen product");
+                    GetMenuItemNoClear(productMenu, "Press escape to exit");
+                    if (currItem == -1)
+                    {
+                        currItem = 0;
+                        break;
+                    }
+                    var currPos = Console.GetCursorPosition();
+                    
+                    Console.SetCursorPosition(currPos.Left, currPos.Top - (productMenu.Length - currItem + 1));
+                    Console.Write("\x1b[K");
+                    Console.Write(productMenu[currItem].Substring(0, productMenu[currItem].IndexOf(':')));
+                    switch (currItem)
+                    {
+                        case 0: products[itemIndex].Date = GetADate("", 0, false); break;
+                        case 1: originalName = products[itemIndex].Name; products[itemIndex].Name = GetString("", false);  break;
+                        case 2: products[itemIndex].Price = GetANumber("", 0, false); break;
+                        case 3: products[itemIndex].Quantity = GetANumber("", 0, false); break;
+                        case 4: originalType = products[itemIndex].Type; products[itemIndex].Type = GetString("", false); break;
+                    }
+                    Parser.ReplaceProduct(originalName, originalType, products[itemIndex]);
+                }
+                currItem = itemIndex + originalOffset;
+            }
+        }
+        private static void DeleteProducts()
+        {
+            Console.Clear();
+            currItem = 1;
+            while (true)
+            {
+                Console.Clear();
+                var products = Parser.ReadProducts();
+                var grouped = products.GroupBy(x => x.Type).OrderBy(g => g.Key);
+
+                var menuItems = new List<string>();
+
+                foreach (var group in grouped)
+                {
+                    menuItems.Add($"[{group.Key}]"); // header
+                    foreach (var product in group.OrderBy(x => x.Name))
+                    {
+                        menuItems.Add("  " + product.Name);
+                    }
+                }
+                int offset = GetMenuItemNoClear(menuItems.ToArray(), "Press escape to exit");
+                if (currItem == -1)
+                {
+                    currItem = 0;
+                    return;
+                }
+
+                Console.Clear();
+                Console.WriteLine($"Delete product {products[currItem].Name} from {products[currItem].Type}");
+                Console.Write("(y/n): ");
+                var inp = Console.ReadLine();
+                if (inp.ToLower() != "no" && inp.ToLower() != "n")
+                    Parser.DeleteProduct(products[currItem]);
+                currItem += offset;
+            }
+        }
+        private static void ExitProgram()
+        {
+            Console.Clear();
+            Console.WriteLine("Thank you for using our program!");
+            Environment.Exit(0);
+        }
+        private static void WaitForSpace()
+        {
+            Console.Write("Press space to exit");
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar) ;
+        }
+        private static void WaitForEsc()
+        {
+            Console.Write("Press escape to exit");
+            while (Console.ReadKey().Key != ConsoleKey.Escape) ;
+        }
+        private static string ReadLineSameLine()
+        {
+            var sb = new StringBuilder();
+            ConsoleKeyInfo key;
+            while ((key = Console.ReadKey(intercept: true)).Key != ConsoleKey.Enter)
+            {
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    if (sb.Length > 0)
+                    {
+                        sb.Remove(sb.Length - 1, 1);
+                        Console.Write("\b \b"); // erase the character visually
+                    }
+                }
+                else
+                {
+                    sb.Append(key.KeyChar);
+                    Console.Write(key.KeyChar);
+                }
+            }
+            return sb.ToString();
+        }
+        private static int GetANumber(string name, int atLeast, bool errorMessage = true)
+        {
+            var pos = Console.GetCursorPosition();
+
+            int num = -1;
+            while (num < atLeast)
+            {
+                try
+                {
+                    Console.Write(name == "" ? ": " : $"{name}: ");
+                    num = int.Parse(!errorMessage ? ReadLineSameLine() : Console.ReadLine());
+                }
+                catch
+                {
+                    if (errorMessage)
+                        Console.WriteLine("The input wasn't a legit number!");
+                    else
+                    {
+                        Console.SetCursorPosition(pos.Left, pos.Top);
+                        Console.Write("\x1b[K");
+                    }
+                    num = -1;
+                }
+            }
+            return num;
+        }
+        private static DateTime GetADate(string name, int atLeast, bool errorMessage = true)
+        {
+            var pos = Console.GetCursorPosition();
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write(name == "" ? ": " : $"{name} (yyyy-MM-dd): ");
+
+                    DateTime date = DateTime.Parse(!errorMessage ? ReadLineSameLine() : Console.ReadLine());
+                    if (date.Year >= atLeast)
+                        return date;
+                    Console.WriteLine($"Date must be from year {atLeast} or later!");
+                }
+                catch
+                {
+                    if (errorMessage)
+                        Console.WriteLine("The input wasn't a legit date!");
+                    else
+                    {
+                        Console.SetCursorPosition(pos.Left, pos.Top);
+                        Console.Write("\x1b[K");
+                    }
+                }
+            } 
+        }
+        private static string GetString(string name, bool errorMessage = true)
+        {
+            string? inp = "";
+            var pos = Console.GetCursorPosition();
+            while (inp == null || inp == "")
+            {
+                Console.Write(name == "" ? ": " : $"Enter the {name}");
+                inp = !errorMessage ? ReadLineSameLine() : Console.ReadLine();
+                if (inp == null || inp == "")
+                {
+                    if (errorMessage)
+                        Console.WriteLine("The input wasn't correct!");
+                    else
+                    {
+                        Console.SetCursorPosition(pos.Left, pos.Top);
+                        Console.Write("\x1b[K");
+                    }
+                }
+            }
+            return inp;
+        }
     }
 }
